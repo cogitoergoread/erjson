54841828b30cac7f8be0f47d401bfc393ad109d5
Jozsef Varga Hupi <31209755+cogitoergoread@users.noreply.github.com>
2025-09-30 18:05:20 +0200

    Buy kész

--- tests/test_csv2bean.sh
+++ tests/test_csv2bean.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-test_success_json_convert() {
+test_success_json_converttra() {
     ctra_convert resources/premium-splitted-tra.csv resources/map-tra.mlr /tmp/
     assert "diff resources/premium-splitted-tra_im.csv /tmp/premium-splitted-tra_im.csv"
     assert "diff resources/premium-splitted-tra.beancount /tmp/premium-splitted-tra.beancount"
+}
+
+test_success_json_convertbuy() {
+    ctra_convert resources/premium-splitted-buy.csv resources/map-buy.mlr /tmp/
+    assert "diff resources/premium-splitted-tra_im.csv /tmp/premium-splitted-tra_im.csv"
 }
 
 
