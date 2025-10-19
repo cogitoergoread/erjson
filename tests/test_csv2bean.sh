f51a02f3b8e70c3594b32aa500c1dc76d1019d23
Jozsef Varga Hupi <31209755+cogitoergoread@users.noreply.github.com>
2025-10-19 12:14:38 +0200

    buy and int, tests as well

--- tests/test_csv2bean.sh
+++ tests/test_csv2bean.sh
@@ -30,6 +30,15 @@ test_eur_bean_converttra() {
     assert "diff resources/eursampl-tra.beancount /tmp/eursampl-tra.beancount"
 }
 
+test_eur_bean_convertbuy() {
+    ctra_convert resources/eursampl-buy.csv resources/meu-buy.mlr /tmp/
+    assert "diff resources/eursampl-buy_im.csv /tmp/eursampl-buy_im.csv"
+}
+
+test_eur_bean_convertint() {
+    ctra_convert resources/eursampl-int.csv resources/meu-int.mlr /tmp/
+    assert "diff resources/eursampl-int_im.csv /tmp/eursampl-int_im.csv"
+}
 
 setup_suite() {
   source ../bin/beangen.sh
